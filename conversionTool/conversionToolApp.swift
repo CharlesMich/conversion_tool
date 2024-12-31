d9f614dd7cbec2e942bcc6ed2d70b54deb08467f
CharlesMich <charleedongo@hotmail.com>
2024-12-31 15:34:00 -0600

    Initial Commit

---
+++ conversionTool/conversionToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  conversionToolApp.swift
+//  conversionTool
+//
+//  Created by Charles Michael on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct conversionToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
